bf2b35a34658e2d14584a3198334f997c062fbb2
WummerMIB <mohr-marco@gmx.de>
2021-11-23 09:47:52 +0100

    push in array gives questionmarks

--- src/Stack.java
+++ src/Stack.java
@@ -1,14 +1,32 @@
+import java.util.Iterator;
 
 public class Stack {
 
-	private char[] stackArray;
-	int stkPtr = 0;
+	char stackArray[] = new char[5];
+	private int stkPtr = 0;
+
 	public char pop() {
-		
-		return (Character) null;
+		char bracketData = stackArray[stkPtr];
+		bracketData = 0;
+		stackArray[stkPtr] = bracketData;
+		stkPtr--;
+		return bracketData;
 	}
-	
+
 	public void push(char c) {
-		
+		System.out.println(c);
+		stackArray[stkPtr] = c;
+		stkPtr++;
+	}
+
+	public void peek() {
+		for (char i : stackArray) {
+			char b = (char) (i + '0');
+			System.out.println(b);
+		}
+	}
+
+	public int getStkPtr() {
+		return this.stkPtr;
 	}
 }
--- src/StringTester.java
+++ src/StringTester.java
@@ -1,50 +1,27 @@
+// 2120717 Marco Mohr, 2123474 Marie Kapusta
+
 import java.sql.Array;
 import java.util.Iterator;
 import java.util.Scanner;
 
-// 2120717 Marco Mohr, 2123474 Marie Kapusta
-public class StringTester {
 
-	
-	
-	
+public class StringTester {
 
 	public static void main(String[] args) {
 		String userInput = inputUserString();
 		char userInputChar;
 		
+		Stack teStack = new Stack();
+		teStack.push('a');
+		teStack.push('b');
+		teStack.peek();
+		
 		for (int i = 0; i < userInput.length(); i++) {
 			userInputChar = userInput.charAt(i);
-			if ('[' == userInputChar) {
-				pushInput(userInputChar);
-			} else if (']' == userInputChar) {
-				popInput();
-			}
-			if ('(' == userInputChar) {
-				pushInput(userInputChar);
-			} else if (')' == userInputChar) {
-				popInput();
-			}
-			if ('{' == userInputChar) {
-				pushInput(userInputChar);
-			} else if ('}' == userInputChar) {
-				popInput();
-			}
 		}
 	}
 
-	public static void pushInput(char userInputChar) {
-		Stack stackBracketOpen = new Stack();
-		stackBracketOpen.push(userInputChar);
-	}
-
-	public static void popInput() {
-		Stack stackBracketOpen = new Stack();
-		stackBracketOpen.pop();
-	}
-
-	public static String inputUserString() {
-		
+	public static String inputUserString() {	
 		Scanner userInputScanner = new Scanner(System.in);
 		String userInput = userInputScanner.nextLine();
 		return userInput;
