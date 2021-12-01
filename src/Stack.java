62a94785252955c075c071516a56ee1b8249695b
WummerMIB <mohr-marco@gmx.de>
2021-12-01 04:50:13 +0100

    added comments

--- src/Stack.java
+++ src/Stack.java
@@ -1,23 +1,37 @@
 import java.util.Iterator;
 
 public class Stack {
-
-	char stackArray[] = new char[100];
+	
+	// Is Stack in there we push brackets and pop them
+	char stackArray[] = new char[1000];
+	// Pointer for the Stack
 	private int stkPtr = 0;
-
+	
+	// pop Method for the Stack that removes the top Element from Stack
 	public char pop() {
+		// decrease pointer by one 
 		stkPtr--;
+		// saves characters in the array to which the 
+		// pointer points and writes them into a variable
 		char bracketData = stackArray[stkPtr];
+		// In Java if you have an empty column its zero so if we make it to zero we empty it  
 		stackArray[stkPtr] = 0;
+		// return char that was in Stack that was pointed at
 		return bracketData;
 	}
 
+	// push Method for the Stack that adds an Element at the Top
 	public void push(char c) {
+		// Writes the passed char variable to the currently shown array element
 		stackArray[stkPtr] = c;
+		// in the end the Pointer gets increased by one to show the next empty Stack Element
 		stkPtr++;
 	}
 
+	// Method that looks if the Stack is empty
 	public boolean isEmpty() {
+		// because you can only push or pop one think add the top if the 
+		// top is empty than return true
 		if (stackArray[0] == 0) {
 			return true;
 		}
--- src/StringTester.java
+++ src/StringTester.java
@@ -3,32 +3,53 @@
 import java.sql.Array;
 import java.util.Iterator;
 import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.jar.Attributes.Name;
+
+import javax.naming.AuthenticationException;
 
 public class StringTester {
 
 	public static void main(String[] args) {
+		// boolean if you want to continue the program 
 		boolean continueTest;
+		// do the program while continueTest is true
 		do {
+			// Shows in Terminal what User should do
 			System.out.println("Bitte geben sie hier ihre Formel ein:");
+			// Saves user input in a String 
 			String inputUserString = inputUserString();
+			// calls Method to show if input is correct and saves answer if correct or wrong in String Variable
 			String outputString = splittArraysIntoCharrs(inputUserString);
+			// shows user if he made a right or wrong input
 			System.out.println(outputString);
+			// calls Method and asks if user wants to continue
 			continueTest = checkIfUserWantsToContinue();
+		// if user wants to stop than continueTest is made to false and than the program ends 
 		} while (continueTest == true);
 	}
 
+	// method for user input in an String 
 	public static String inputUserString() {
+		// Scanner Method for Input
 		Scanner userInputScanner = new Scanner(System.in);
+		// writes text in String
 		String userInput = userInputScanner.nextLine();
+		// return user input
 		return userInput;
 	}
 
+	// Method that split string into char and than pops push 
+	// into Stack and checks if input was right or wrong
 	public static String splittArraysIntoCharrs(String userInput) {
+		// initialize an Object from Stack to call method pop, push and is empty
 		Stack pushStack = new Stack();
+		// for loop to split string into chars 
 		for (int i = 0; i < userInput.length(); i++) {
+			// saves individuell char from Input in an variable
 			char userInputChar = userInput.charAt(i);
 
+			// if input is (, [ or { than push it in the Stack
 			if (userInputChar == '(') {
 				pushStack.push(userInputChar);
 			}
@@ -39,6 +60,9 @@ public class StringTester {
 				pushStack.push(userInputChar);
 			}
 
+			// if user input is ),] or } than check if the Stack is empty because if yes than input is false
+			// than if the Stack is not empty pop the last input and check if the returned char is right with the 
+			// bracket for example () the pop returns ( so it is right if it returns [{ its wrong 
 			if (userInputChar == ')') {
 				boolean checkEmpty = pushStack.isEmpty();
 				if (checkEmpty == true) {
@@ -73,6 +97,8 @@ public class StringTester {
 				}
 			}
 		}
+		// at the end if Stack is empty than the input is correct but 
+		// if there is something on the Stack than return wrong
 		boolean checkEmpty = pushStack.isEmpty();
 		if (checkEmpty == false) {
 			return "Klammern ungültig";
@@ -83,11 +109,17 @@ public class StringTester {
 
 		return "";
 	}
-
+	
+	
+	// Method to ask if user wants to continue
 	public static boolean checkIfUserWantsToContinue() {
+		// Shows in Terminal what User should do
 		System.out.println("Drücken sie Ja/ja um weiter zu machen oder irgendeine Taste um zu beenden");
+		// calls Method that allows user to make an input and save it an string
 		String input = inputUserString();
+		// boolean if user wants to continue
 		boolean continueFormalCheck;
+		// if input is ja or Ja than continue everything else ends the program
 		if (input.equals("ja") || input.equals("Ja")) {
 			return continueFormalCheck = true;
 		} else {
